082f3fff7925dd07cdbb4b71ad7cbb8970414120
ecunha1996 <id9411@alunos.uminho.pt>
2023-11-11 16:07:53 +0000

    [ADD] optimization_docker.py

---
+++ src/GSMMutils/optimization/optimization_docker.py
@@ -0,0 +1,18 @@
+import os
+from os.path import join
+from GSMMutils.utils.remote import Remote
+
+
+class DFBA:
+    def __init__(self, data_directory=None, src_directory=None):
+        self.data_directory = data_directory or join(os.path.dirname(os.path.abspath(__file__)).split("src")[0], "data")
+        self.src_directory = src_directory or join(os.path.dirname(os.path.abspath(__file__)).split("src")[0], "src")
+        self.remote = Remote(data_directory, src_directory)
+
+    def run(self):
+        cmd = (f'podman run --name optimization -v {self.data_directory}:/home/data/:Z -v '
+               f'{self.src_directory}:/home/src/:Z davidtourigny/dfba '
+               f'sh -c "/usr/bin/python3 -m pip install --upgrade pip && pip install -q --root-user-action=ignore mewpy matplotlib seaborn scipy '
+               f'joblib openpyxl psutil plotly kaleido timeout-decorator tqdm parallelbar && '
+               f'python3 /home/examples/optimization/optimization.py > /home/data/output.txt"')  #
+        self.remote.run('optimization', cmd)
