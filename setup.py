500d0e59137dee4d81d6f8060b99549030650926
ecunha1996 <id9411@alunos.uminho.pt>
2024-01-11 11:38:07 +0000

    [FIX] setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
 if __name__ == "__main__":
-    setup()
+    setup(name='gsmmutils',
+    version='0.0.1',
+    python_requires='>=3.6',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    package_data={"": ["*.xml", "*.csv", "*.txt"], 'gsmmutils': files},
+    include_package_data=True,
+    long_description=readme,
+    test_suite='tests'
+              )
